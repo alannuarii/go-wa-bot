5c0752988dec8451cf2c61f1ba76cac9f93188c9
alannuarii <alannuariii@gmail.com>
2024-06-10 07:25:39 +0800

    Update

--- go.mod
+++ go.mod
@@ -21,5 +21,5 @@ require (
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	google.golang.org/protobuf v1.34.1
 )
--- main.go
+++ main.go
@@ -7,59 +7,81 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
-	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/protobuf/proto"
 )
 
-func eventHandler(evt interface{}) {
-	switch v := evt.(type) {
-	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
-	}
+
+func eventHandler(evt interface{}, client *whatsmeow.Client) {
+    switch v := evt.(type) {
+    case *events.Message:
+        fmt.Println("Received a message!", v.Message.GetConversation())
+
+        if v.Message.GetConversation() == "Hai" {
+
+			jid := types.NewJID("120363204122320229", "g.us")
+
+			msg := &waProto.Message{
+				Conversation: proto.String("This is response")}
+            
+            _, err := client.SendMessage(context.Background(), types.JID(jid), msg)
+            if err != nil {
+                fmt.Println("Error sending message:", err)
+            } else {
+                fmt.Println("Message sent successfully")
+            }
+        }
+    }
 }
 
+
 func main() {
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	container, err := sqlstore.New("sqlite3", "file:gowabot.db?_foreign_keys=on", dbLog)
-	if err != nil {
-		panic(err)
-	}
+    dbLog := waLog.Stdout("Database", "DEBUG", true)
+    container, err := sqlstore.New("sqlite3", "file:gowabot.db?_foreign_keys=on", dbLog)
+    if err != nil {
+        panic(err)
+    }
 
-	deviceStore, err := container.GetFirstDevice()
-	if err != nil {
-		panic(err)
-	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
+    deviceStore, err := container.GetFirstDevice()
+    if err != nil {
+        panic(err)
+    }
+    clientLog := waLog.Stdout("Client", "DEBUG", true)
+    client := whatsmeow.NewClient(deviceStore, clientLog)
+    client.AddEventHandler(func(evt interface{}) {
+        eventHandler(evt, client)
+    })
 
-	if client.Store.ID == nil {
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-		for evt := range qrChan {
-			if evt.Event == "code" {
+    if client.Store.ID == nil {
+        qrChan, _ := client.GetQRChannel(context.Background())
+        err = client.Connect()
+        if err != nil {
+            panic(err)
+        }
+        for evt := range qrChan {
+            if evt.Event == "code" {
 
-				fmt.Println("QR code generated: ", evt.Code)
-			} else {
-				fmt.Println("Login event:", evt.Event)
-			}
-		}
-	} else {
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-	}
+                fmt.Println("QR code generated: ", evt.Code)
+            } else {
+                fmt.Println("Login event:", evt.Event)
+            }
+        }
+    } else {
+        err = client.Connect()
+        if err != nil {
+            panic(err)
+        }
+    }
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    <-c
 
-	client.Disconnect()
+    client.Disconnect()
 }
